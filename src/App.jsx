586107bb99c60d5c82dc36b70a141e80dbbc497c
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-10-06 19:33:11 -0700

    change to football scores

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,41 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  // This is just like:
+  // var count = 0;
   const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0)
+  const [lives, setLives] = useState(3)
+  const [timeouts, setTimeouts] = useState(3);
+
+  function extraPoint() {
+    // count = count + 1
+    setCount(count + 1)
+  }
+  function safety() {
+    // count = count + 1
+    setCount(count + 2)
+  }
+  function touchdown() {
+    // count = count + 1
+    setCount(count + 6)
+  }
+  function fieldGoal() {
+    // count = count + 1
+    setCount(count + 3)
+  }
+  function decrement3() {
+    // count = count + 1
+    setCount(count - 3)
+  }
+  function reset() {
+    setCount(0);
+  }
+
 
   return (
     <>
+      <p>Timeouts: {timeouts}</p>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,14 +47,31 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <p>{count}</p>
+      <h1>Vite + React {count}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={extraPoint}>
+          Extra point
+        </button>
+        <button onClick={safety}>
+          safety 
+        </button>
+        <button onClick={touchdown}>
+          touchdown 
+        </button>
+        <button onClick={fieldGoal}>
+          field goal 
+        </button>
+        <button onClick={decrement3}>
+          Decrement by 3
+        </button>
+        <button onClick={reset}>
+          Reset
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>{count}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
